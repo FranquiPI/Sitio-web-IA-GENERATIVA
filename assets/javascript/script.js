7c91b27d8a2559ae0851d1a2b0c1e70f91e93122
FranquiPI <pizarrofranco25@gmail.com>
2025-02-22 22:12:26 -0300

    Modifiamos todo para hacer primero el mobile design first

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById('menu__btn')
+const nav = document.querySelector('.nav-bar')
+
+
+menu.addEventListener('click', () =>{
+    nav.classList.toggle('active');
+    
+    
+})
